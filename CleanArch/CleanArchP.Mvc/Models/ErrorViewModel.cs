4a7487ebd59f08aba0f5613f07e5c8f2d1e48122
HenryK23 <59871978+HenryK23@users.noreply.github.com>
2021-12-11 20:52:21 +0000

    Initial Commit for CleanArch.Mvc Project (Presentation)

---
+++ CleanArch/CleanArchP.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CleanArchP.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
